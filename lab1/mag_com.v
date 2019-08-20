5a27ea4e028a941b30a958a7adbf35a658dea740
Rohit Rajhans <rohitrajhans07@gmail.com>
2019-08-20 16:01:53 +0530

    correcting mag_com.v

--- lab1/mag_com.v
+++ lab1/mag_com.v
@@ -2,29 +2,29 @@
 
 module mag_beh(a, b, c);
 
-	input [3:0] a;
-	input [3:0] b;
+	input [3:0] a, b;
 	output [2:0]c;
-	//reg c;
-	always@(a or b)
-		if(a==b) c=3b'100;
-		if(a>b)	c=3b'010;
-		else	c=3b'001;
-		
+	reg c;
+	always@ (a or b)
+		begin
+			if(a==b) c=3'b100;
+			else if(a>b)	c=3'b010;
+			else if(a<b)	c=3'b001;
+		end
 endmodule
 
 module testbench;
-	reg [3:0]a, [3:0]b;
-	wire [2:0] c;
+	reg [3:0]a, b;
+	wire [2:0]c;
 	
-	mag_com mag_beh(a, b, c);
+	mag_beh mag_com(a, b, c);
 	initial
 		begin
 			$monitor(, $time, " a=%4b, b=%4b, c=%3b", a, b, c);
-			#0 a=4b'0000; b=4b'0000;
-			#5 a=4b'0001; b=4b'0000;
-			#10 a=4b'0000; b=4b'0001;
-			#100 $finish
+			#0 a=4'b0000; b=4'b0000;
+			#5 a=4'b0001; b=4'b0000;
+			#10 a=4'b0000; b=4'b0001;
+			#100 $finish;
 		end
 		
 endmodule
